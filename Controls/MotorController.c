39767163ac3da08e37f433df34125ca781b836ae
Sidharth Babu <sidharth.n.babu@utexas.edu>
2020-08-14 16:57:04 -0500

    Function written to open file, write necessary message, and close

--- Controls/MotorController.c
+++ Controls/MotorController.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
 void CANWriter ( int CAN_ID, int CAN_MESSAGE) {
-    
+    FILE *CANFILE = fopen("CAN.csv","w+");
+    fprintf(CANFILE,"0x%d,%d",CAN_ID,CAN_MESSAGE);
+    fclose(CANFILE);
 }
