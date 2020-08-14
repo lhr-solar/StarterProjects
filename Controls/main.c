d054c2c25a497b3f650e2da4715e49760e9b89ac
Sidharth Babu <sidharth.n.babu@utexas.edu>
2020-08-14 16:55:44 -0500

    Delay function to delay by one second each time; Call to CAN writing function

--- Controls/main.c
+++ Controls/main.c
@@ -1,1 +1,17 @@
 #include "MotorController.h"
+#include "stdlib.h"
+#include <time.h>
+
+void delay(int seconds){
+    int *mseconds;
+    mseconds = seconds*1000;
+    clock_t start_time = clock();
+    while (clock()<start_time+mseconds){
+        ;
+    }
+}
+
+void main(){
+    delay(1);
+    CANWriter(221,rand());
+}
