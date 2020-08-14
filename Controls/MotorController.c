2a3fddf06360f0217ef4a776874081384a06ebb0
Sidharth Babu <sidharth.n.babu@utexas.edu>
2020-08-14 17:20:49 -0500

    Fixed make issue

--- Controls/MotorController.c
+++ Controls/MotorController.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "MotorController.h"
+
 void CANWriter ( int CAN_ID, int CAN_MESSAGE) {
-    FILE *CANFILE = fopen("CAN.csv","w+");
+    FILE *CANFILE;
+    CANFILE = fopen("CAN.csv","w+");
     fprintf(CANFILE,"0x%d,%d",CAN_ID,CAN_MESSAGE);
     fclose(CANFILE);
 }
