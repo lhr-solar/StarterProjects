42fc85b8f053ee7204b5c314f0d6e8ee3595ecee
Sidharth Babu <sidharth.n.babu@utexas.edu>
2020-08-24 16:04:25 -0500

    Fixed int casting issue for delay function

--- Controls/main.c
+++ Controls/main.c
@@ -2,16 +2,16 @@
 #include "stdlib.h"
 #include <time.h>
 
-void delay(int seconds){
-    clock_t start_time = clock();
-    while ((clock()/(CLOCKS_PER_SEC))<start_time+seconds){
+void delay(int mseconds){
+    clock_t start_time = (clock()/CLOCKS_PER_SEC)*1000; //(clock()/clocks_per_sec) = seconds -> (clock()/clocks_per_sec)*1000 = milliseconds
+    while (((clock()/CLOCKS_PER_SEC)*1000)<start_time+mseconds){
         ;
     }
 }
 
 void main(){
     while(1){
-        delay(.250);
+        delay(250);
         CANWriter(221,rand());
     }
 }
