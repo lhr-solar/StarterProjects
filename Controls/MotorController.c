82d0db701eb00ee4ab3fef9b5b65943abd490279
AleNeff <alejandro.neff@utexas.edu>
2020-09-05 14:50:52 -0500

    fixes

--- Controls/MotorController.c
+++ Controls/MotorController.c
@@ -3,7 +3,7 @@
 
 void Motor_Control(uint16_t CAN_ID, unsigned long long int CAN_MESSAGE) {
     FILE *fp = fopen("CAN.csv", "w");
-    fprintf(fp, "%hu, %llu", CAN_ID, CAN_MESSAGE);
+    fprintf(fp, "%hu,%llu", CAN_ID, CAN_MESSAGE);
     fclose(fp);
-    //printf("%hu, %llu\n", CAN_ID, CAN_MESSAGE);
+    printf("%hu, %llu\n", CAN_ID, CAN_MESSAGE);
 }
--- Controls/simulate.py
+++ Controls/simulate.py
@@ -1,7 +1,9 @@
 import csv
+import time
 
 with open('CAN.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     while True:
         for row in csv_reader:
             print("ID: " + (str)(row[0]) + " Message:" + (str)(row[1]), end='\r')
+            time.sleep(1)
