0b07a2eeac9b738b3149da2fc492abb220f789ce
AleNeff <alejandro.neff@utexas.edu>
2020-09-05 16:16:18 -0500

    Finished2.0

--- Controls/simulate.py
+++ Controls/simulate.py
@@ -1,9 +1,8 @@
 import csv
 import time
 
-with open('CAN.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    while True:
+while True:
+    with open('CAN.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             print("ID: " + (str)(row[0]) + " Message:" + (str)(row[1]), end='\r')
-            time.sleep(1)
