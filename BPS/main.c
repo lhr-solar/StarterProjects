b1217fa445178755de9d5cffbf30bd0fdaa545de
harshigorla <raghuharshitha12@gmail.com>
2020-10-01 13:21:31 -0500

    Starter project code, only modified the main file.

--- BPS/main.c
+++ BPS/main.c
@@ -3,8 +3,22 @@
 
 int main(void){
 	//TODO: initialization here
+	currentInit();
+	tempInit();
+	voltInit();
+	contactorInit();
 	while(1){
 		//TODO: main loop here
+		currentRead();
+		voltRead();
+		tempRead();
+
+		if (current > MAX_CURRENT || voltage > MAX_VOLTAGE || voltage < MIN_VOLTAGE || temperature > MAX_TEMP) {
+			setContactor(false);
+		}
+		else {
+			setContactor(true); //if conditions are acceptable, contactor turns on again
+		}
 	}
 	return 0;
 }
