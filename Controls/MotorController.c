1a56c5f55ea6edecc6786e575fe4edce5c2c628d
tasniabari <tasniabari@utexas.edu>
2021-09-11 15:02:25 -0500

    commit

--- Controls/MotorController.c
+++ Controls/MotorController.c
@@ -1,21 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include "MotorController.h"
-#include "stdlib.h"
-#include <time.h>
 
 
-void timeDelay(int mSeconds){
-    clock startTime = (clock()/CLOCKS_PER_SEC)*1000; //convert to milliseconds
-    while (((clock()/CLOCKS_PER_SEC)*1000)<startTime+mSeconds)
-    {
-        ; //this makes the clock run to create a delay
-    }
-}
 
-void main()
-{
-    while(1)
-    {
-        timeDelay(100);
-        MotorController_Write_to_CAN(221,rand()); //send a random # as a message every time 
-    }
+void MotorController_Write_to_CAN(int CAN_ID, int CAN_MESSAGE) {
+    FILE *CanFile = fopen("CAN.csv","w+");
+    fprintf(CanFile,"0x%d,%d",CAN_ID,CAN_MESSAGE);
+    fclose(CanFile);
 }
