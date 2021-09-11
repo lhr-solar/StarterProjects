8dedbbdf5b29c04d37647933e36303f8e35d3825
tasniabari <70384737+tasniabari@users.noreply.github.com>
2021-09-11 11:56:54 -0500

    Update MotorController.c

---
+++ Controls/MotorController.c
@@ -0,0 +1,21 @@
+#include "MotorController.h"
+#include "stdlib.h"
+#include <time.h>
+
+
+void timeDelay(int mSeconds){
+    clock startTime = (clock()/CLOCKS_PER_SEC)*1000; //convert to milliseconds
+    while (((clock()/CLOCKS_PER_SEC)*1000)<startTime+mSeconds)
+    {
+        ; //this makes the clock run to create a delay
+    }
+}
+
+void main()
+{
+    while(1)
+    {
+        timeDelay(100);
+        MotorController_Write_to_CAN(221,rand()); //send a random # as a message every time 
+    }
+}
