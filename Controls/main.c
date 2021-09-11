b3a77a0a1ae9eda2cffcd722724d6564283bc4fb
tasniabari <70384737+tasniabari@users.noreply.github.com>
2021-09-11 14:51:07 -0500

    Update main.c

--- Controls/main.c
+++ Controls/main.c
@@ -7,7 +7,7 @@ void timeDelay(int miliseconds){
     clock_t time = (clock()/CLOCKS_PER_SEC)*1000; //convert to milliseconds
     while (((clock()/CLOCKS_PER_SEC)*1000) < time + miliseconds)
     {
-        ; //this makes the clock run to create a delay
+        ; //this makes the clock run to create a delay, no code here
     }
 }
 
@@ -15,7 +15,7 @@ void main()
 {
     while(1)
     {
-        timeDelay(100);
-        MotorController_Write_to_CAN(221,rand()); //send a random # as a message every time to the ID 221
+        timeDelay(100); //prevents the below method from constantly writing to the csv file
+        MotorController_Write_to_CAN(221,rand()); //send a random # to the CAN.csv file as a message and the ID 221. These are in the CAN.csv file
     }
 }
