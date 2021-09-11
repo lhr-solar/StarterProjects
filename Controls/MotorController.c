7bf2472f1dab84b6bd35113d754dfc3864fa6a41
tasniabari <70384737+tasniabari@users.noreply.github.com>
2021-09-11 13:52:45 -0500

    Update MotorController.c

--- Controls/MotorController.c
+++ Controls/MotorController.c
@@ -1,21 +1,14 @@
+  
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include "MotorController.h"
-#include "stdlib.h"
-#include <time.h>
 
 
-void timeDelay(int mSeconds){
-    clock_t startTime = (clock()/CLOCKS_PER_SEC)*1000; //convert to milliseconds
-    while (((clock()/CLOCKS_PER_SEC)*1000)<startTime+mSeconds)
-    {
-        ; //this makes the clock run to create a delay
-    }
-}
 
-void main()
-{
-    while(1)
-    {
-        timeDelay(100);
-        MotorController_Write_to_CAN(221,rand()); //send a random # as a message every time 
-    }
+void MotorController_Write_to_CAN( uint16_t CAN_ID, int CAN_MESSAGE) {
+    FILE *CanFile;
+    CanFile = fopen("CAN.csv","w+");
+    fprintf(CanFile,"0x%d,%d",CAN_ID,CAN_MESSAGE);
+    fclose(CanFile);
 }
