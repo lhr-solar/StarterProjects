ee4caa886c3e85eb6ea2a57b15faf6ac8ffe8eb1
tasniabari <70384737+tasniabari@users.noreply.github.com>
2021-09-11 14:15:08 -0500

    Update MotorController.c

--- Controls/MotorController.c
+++ Controls/MotorController.c
@@ -6,7 +6,7 @@
 
 
 
-void MotorController_Write_to_CAN( uint16_t CAN_ID, int CAN_MESSAGE) {
+void MotorController_Write_to_CAN(int CAN_ID, int CAN_MESSAGE) {
     FILE *CanFile;
     CanFile = fopen("CAN.csv","w+");
     fprintf(CanFile,"0x%d,%d",CAN_ID,CAN_MESSAGE);
