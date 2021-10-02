5b3763394939a6cf3abc23b23820550e950d6b1a
Yejean Kim <kimyejean02@gmail.com>
2021-10-02 10:24:22 -0500

    reads cleaned data into pandas dataframe

--- DataAcq/clean.py
+++ DataAcq/clean.py
@@ -1,15 +1,17 @@
 """Module to clean the data in the CSV file
 """
+import pandas
 from pandas import DataFrame
 
-def clean() -> DataFrame:
-    """TODO: Implement this
-    """
-    pass
+USD_PER_EURO = 1.17
 
+def clean() -> DataFrame:
+    books = pandas.read_csv("book_file.csv", encoding = "ISO-8859-1")
+    top_rated = books[(books["rating"].notnull()) & (books["rating"] != "Two") & (books["rating"] != "One") & (books["rating"] != "Zero")]
+    top_rated["usd_price"] = books.price * USD_PER_EURO
+    top_rated.drop(columns = "price", inplace = True)
+    #print(top_rated)
+    return top_rated
 
 if __name__ == "__main__":
-    """TODO: Write code here to test this
-    particular module on its own
-    """
-    pass
+    clean()
