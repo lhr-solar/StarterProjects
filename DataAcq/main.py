3d6b019665c66c5d4604ff087a6c8dd2ee8f1b7e
Yejean Kim <kimyejean02@gmail.com>
2021-10-02 10:55:26 -0500

    scrapes, cleans, and plots data

--- DataAcq/main.py
+++ DataAcq/main.py
@@ -1,10 +1,12 @@
 """Main engine to tie all the pieces together
 """
+import scrape
+import clean
+import visualize
 
 def main():
-    """TODO: Implement this
-    """
-    pass
+    scrape.scrape()
+    visualize.visualize(clean.clean(), clean.clean().axes[1][0], clean.clean().axes[1][3])
 
 
 if __name__ == "__main__":
