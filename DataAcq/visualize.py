ed70f5cdbd478ebcb34014e4cd24da73b790ab87
Yejean Kim <kimyejean02@gmail.com>
2021-10-02 10:52:50 -0500

    plots pandas data as scatter plot

--- DataAcq/visualize.py
+++ DataAcq/visualize.py
@@ -1,15 +1,14 @@
 """Module to visualize data from a pandas dataframe
 """
 from pandas import DataFrame
+import matplotlib.pyplot as plt
 
 def visualize(data: DataFrame, x_col: str, y_col: str):
-    """TODO: Implement this
-    """
-    pass
+    plt.scatter(data[x_col], data [y_col])
+    plt.xlabel(x_col)
+    plt.ylabel(y_col)
+    plt.show()
 
 
 if __name__ == "__main__":
-    """TODO: Write code here to test this
-    particular module on its own
-    """
     pass
