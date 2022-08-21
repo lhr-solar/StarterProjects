f99ead0b851ccadb7c32e559dbd1f43475b05c59
IshDeshpa <ishdeshpa@gmail.com>
2022-08-21 19:28:42 +0000

    added pointers as parameters to Customer1_checkout and Customer2_checkout

--- Controls/Cafe/Customer1.c
+++ Controls/Cafe/Customer1.c
@@ -4,23 +4,25 @@
  * Customer 1 wants to buy something!
  */
 
-
 /**
- * @brief Get the FOURTH item and cost on the menu (Cafe.h). Print the value of this item,
- * along with the corresponding cost. Add the cost to the business revenue and print the current business revenue. 
- * 
- * In this method, use POINTER MATH to access values in both arrays.
+ * @brief Get the FOURTH item and cost on the menu (Cafe.h). Return the name and cost
+ * of the item to the pointers passed into the function. Add the cost to the business 
+ * revenue and print the current business revenue.
  */
-void Customer1_checkout(void) {
+void Customer1_checkout(char* name, int* cost) {
 	OS_ERR err; // Make sure to check for errors and print the error code if not OS_ERR_NONE
 }
 
 /**
- * @brief Acquire the register. Call checkout. After checking out, release the register.
- * Delay for FOUR seconds. Loop.
+ * @brief Acquire the register. Call checkout. Print the name and cost of the item.
+ * After checking out, release the register. Delay for FOUR seconds. Loop.
  */
 void Task_Customer_1(void* p_arg) {
 	OS_ERR err;	// Make sure to check for errors and print the error code if not OS_ERR_NONE
+	
+	// Use these pointers as parameters for Customer1_checkout
+	char* name;
+	int* cost;
 	while(1){
 		
 	}
--- Controls/Cafe/Customer2.c
+++ Controls/Cafe/Customer2.c
@@ -4,24 +4,26 @@
  * Customer 2 wants to buy something!
  */
 
-
 /**
- * @brief Get the FIRST item and cost on the menu (Cafe.h). Print the value of this item,
- * along with the corresponding cost. Add the cost to the business revenue and print the current business revenue.
- * 
- * In this method, use ARRAY INDEXING to access values in both arrays.
+ * @brief Get the FIRST item and cost on the menu (Cafe.h). Return the name and cost
+ * of the item to the pointers passed into the function. Add the cost to the business 
+ * revenue and print the current business revenue.
  */
-void Customer2_checkout(void) {
+void Customer2_checkout(char* name, int* cost) {
 	OS_ERR err; // Make sure to check for errors and print the error code if not OS_ERR_NONE
 }
 
 /**
- * @brief Acquire the register. Call checkout. After checking out, release the register.
- * Wait for THREE seconds. Loop.
+ * @brief Acquire the register. Call checkout. Print the name and cost of the item.
+ * After checking out, release the register. Delay for THREE seconds. Loop.
  */
 void Task_Customer_2(void* p_arg) {
 	OS_ERR err;	// Make sure to check for errors and print the error code if not OS_ERR_NONE
+
+	// Return the name and cost into these pointers from Customer2_checkout
+	char* name;
+	int* cost;
 	while(1){
-		
+
 	}
 }
