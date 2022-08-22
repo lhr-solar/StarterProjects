842139576ee264842bad649476eed97757274059
Cam0Cow <roie.roiegal@gmail.com>
2022-08-22 17:45:06 +0000

    implemented recipient

--- Controls/Mail/Recipient.c
+++ Controls/Mail/Recipient.c
@@ -10,6 +10,9 @@
  */
 void readMail(void) {
 	OS_ERR err; // Make sure to check for errors and print the error code if not OS_ERR_NONE
+	printf("Mailbox contains: \"%s\"\n", mailbox);
+	OSTimeDlyHMSM(0, 0, 2, 0, OS_OPT_TIME_HMSM_STRICT, &err);
+	// TODO: error handling
 }
 
 /**
@@ -17,7 +20,10 @@ void readMail(void) {
  */
 void Task_Recipient(void* p_arg) {
 	OS_ERR err;	// Make sure to check for errors and print the error code if not OS_ERR_NONE
+	CPU_TS ts;
 	while(1){
-		
+		OSSemPend(&MailboxFlag_Sem4, 0, OS_OPT_PEND_BLOCKING, &ts, &err);
+		// TODO: error handling
+		readMail();
 	}
 }
