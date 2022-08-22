f0784a6f9936b5208c46a6d4850c7dd9d5ebb882
Cam0Cow <roie.roiegal@gmail.com>
2022-08-22 19:26:56 +0000

    Implemented customer 1

--- Controls/Cafe/Customer1.c
+++ Controls/Cafe/Customer1.c
@@ -9,8 +9,18 @@
  * of the item to the pointers passed into the function. Add the cost to the business 
  * revenue and print the current business revenue.
  */
-void Customer1_checkout(char* name, int* cost) {
+void Customer1_checkout(char** name, int* cost) {
 	OS_ERR err; // Make sure to check for errors and print the error code if not OS_ERR_NONE
+	CPU_TS ts;
+	*name = menu[3];
+	*cost = costs[3];
+
+	OSMutexPend(&RegisterOccupied_Mutex, 0, OS_OPT_PEND_BLOCKING, &ts, &err);
+	// TODO: error checking
+	revenue += *cost;
+	printf("Current business revenue: %d\n", revenue);
+	OSMutexPost(&RegisterOccupied_Mutex, OS_OPT_POST_NONE, &err);
+	// TODO: error checking
 }
 
 /**
@@ -22,8 +32,11 @@ void Task_Customer_1(void* p_arg) {
 	
 	// Use these pointers as parameters for Customer1_checkout
 	char* name;
-	int* cost;
+	int cost;
 	while(1){
-		
+		Customer1_checkout(&name, &cost);
+		print("Customer 1 just bought %s for $%d\n", name, cost);
+		OSTimeDlyHMSM(0, 0, 4, 0, OS_OPT_TIME_HMSM_STRICT, &err);
+		// TODO: error checking
 	}
 }
