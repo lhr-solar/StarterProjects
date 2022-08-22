522b0683994b4c0691feb7344a5aa53451fc2d46
Cam0Cow <roie.roiegal@gmail.com>
2022-08-22 17:40:56 +0000

    Implemented mailman

--- Controls/Mail/Mailman.c
+++ Controls/Mail/Mailman.c
@@ -1,4 +1,5 @@
 #include "Mail.h"
+#include <stdlib.h>
 
 /**
  * The mailman will put mail in the mailbox.
@@ -6,11 +7,20 @@
 
 
 /**
- * @brief Puts a letter in the mailbox (declared in Tasks.h). The letter must be unique every time
+ * @brief Puts a letter in the mailbox (declared in Mail.h). The letter must be unique every time
  * this function is called (suggestion: include a random number!). Print "Finished!" at the end.
  */
 void depositLetter(void) {
 	OS_ERR err; // Make sure to check for errors and print the error code if not OS_ERR_NONE
+
+	int len = (random() % 6) + 3; // Pick a word length between 3 and 8 letters
+	for (int i=0; i<len; i++) {
+		mailbox[i] = (random() % 26) + 'a';
+	}
+	mailbox[len] = '\0';
+	OSSemPost(&MailboxFlag_Sem4, OS_OPT_POST_ALL, &err);
+	// TODO: error handling
+	printf("Finished!\n");
 }
 
 /**
@@ -20,6 +30,8 @@ void depositLetter(void) {
 void Task_Mailman(void* p_arg) {
 	OS_ERR err;	// Make sure to check for errors and print the error code if not OS_ERR_NONE
 	while(1){
-
+		depositLetter();
+		OSTimeDlyHMSM(0, 0, 3, 0, OS_OPT_TIME_HMSM_STRICT, &err);
+		// TOOD: error handling
 	}
 }
