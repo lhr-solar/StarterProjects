b4b9c015dc3d4ef275b934417d4a7b6b1130d664
Madeleine Lee <madeleinercflee@gmail.com>
2022-09-24 16:55:54 +0000

    Added a "watermark stack limit" of 128 (half of stack size) to Tasks.h Attempted to complete the test file for Mail, though error checking still not finished Will now try to actually fun files.

--- Controls/Tests/Test_Mail.c
+++ Controls/Tests/Test_Mail.c
@@ -8,7 +8,8 @@
  */
 
  // Mailbox semaphore, required by the mailman and recipient tasks
-OS_SEM MailboxFlag_Sem4;
+OS_SEM MailboxFlag_Sem4; // Declare and allocate space for the mailbox semaphore I think
+// But why is it both here and in the header file? What's the difference?
 
 // Mailbox contents (allocated as 256 characters long, initialized to be empty)
 char mailbox[256] = {'\0'};
@@ -19,11 +20,27 @@ int main(void){
 	OSInit(&err);	// Initialize the OS
 	OS_CPU_SysTickInit();
 	
+	// I probably should check err here first
+
 	// Create any semaphores/mutexes and initialize any global variables here
-	
+	OSSemCreate(&MailboxFlag_Sem4, "Mailbox Semaphore", 0, &err); // Create Mailbox sem
 
 	// Initialize both tasks here
-	
+	OSTaskCreate(
+		(OS_TCB*)&Mailman_TCB, 
+		(CPU_CHAR*)"Mailman",
+		(OS_TASK_PTR)Task_Mailman,
+		(void*)NULL,
+		(OS_PRIO)TASK_MAILMAN_PRIO,
+		(CPU_STK*)Mailman_Stk,
+		(CPU_STK_SIZE)WATERMARK_STACK_LIMIT,
+		(CPU_STK_SIZE)TASK_MAILMAN_STACK_SIZE,
+		(OS_MSG_QTY)0,
+		(OS_TICK)0,
+		(void*)NULL,
+		(OS_OPT)(OS_OPT_TASK_STK_CLR),
+		(OS_ERR*)&err
+	);
 
 	OSStart(&err);	// Start the OS
 
