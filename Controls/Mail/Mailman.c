5eee866a8b3d967ca43eee98441719a0f1bf13ce
Audrey Pick <audreyg_pick@utexas.edu>
2022-09-24 22:29:35 +0000

    Finished Stater Project!

--- Controls/Mail/Mailman.c
+++ Controls/Mail/Mailman.c
@@ -1,4 +1,6 @@
 #include "Mail.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * The mailman will put mail in the mailbox.
@@ -11,8 +13,21 @@
  * Once the letter is deposited, signal the semaphore (effectively putting up the flag).
  * Print "Finished!" at the end.
  */
+//extern char* mailbox;
 void depositLetter(void) {
 	OS_ERR err; // Make sure to check for errors and print the error code if not OS_ERR_NONE
+	//int i;
+	//char max=random()%255;//different lengths at diff times
+	for(int i=0;i<255;i++){
+		mailbox[i]=64+rand()%63;
+	}
+	mailbox[256]=0;//null at end
+	OSSemPost(&MailboxFlag_Sem4, OS_OPT_POST_NONE, &err);//post +1 mail in box
+		if(err!=OS_ERR_NONE){
+			printf("Error from OSSemPost Mailman, depositLetter");
+			return;
+		}
+	printf("Finished!\n");
 }
 
 /**
@@ -21,7 +36,16 @@ void depositLetter(void) {
  */
 void Task_Mailman(void* p_arg) {
 	OS_ERR err;	// Make sure to check for errors and print the error code if not OS_ERR_NONE
+	//CPU_TS ts; 
+	
+	CPU_INT16U seconds = 3;//3000ms = 3s DELAY
+	OS_OPT opt =OS_OPT_TIME_HMSM_STRICT;
 	while(1){
-
+		depositLetter();
+		OSTimeDlyHMSM(0,0,seconds,0, opt,&err);
+		if(err!=OS_ERR_NONE){
+			printf("Error from OSTimeDlyHMSM Task_Mailman");
+			return;
+		}
 	}
 }
--- Controls/Mail/Recipient.c
+++ Controls/Mail/Recipient.c
@@ -1,4 +1,5 @@
 #include "Mail.h"
+#include <stdio.h>
 
 /**
  * The recipient will read mail from the mailbox.
@@ -10,6 +11,13 @@
  */
 void readMail(void) {
 	OS_ERR err; // Make sure to check for errors and print the error code if not OS_ERR_NONE
+	printf("%s\n",mailbox);
+	OS_OPT opt =OS_OPT_TIME_HMSM_STRICT;
+	OSTimeDlyHMSM(0,0,2,0, opt,&err);//TWO seconds
+		if(err!=OS_ERR_NONE){
+			printf("Error from OSTimeDlyHMSM Recipient, readMail");
+			return;
+		}
 }
 
 /**
@@ -17,7 +25,19 @@ void readMail(void) {
  */
 void Task_Recipient(void* p_arg) {
 	OS_ERR err;	// Make sure to check for errors and print the error code if not OS_ERR_NONE
+	CPU_TS timestamp;
 	while(1){
-		
+		OSSemPend(
+			&MailboxFlag_Sem4,
+			0,
+			OS_OPT_PEND_BLOCKING,
+			&timestamp,
+			&err
+			);//pending on mail--is ther any mail yet??
+		if(err!=OS_ERR_NONE){
+			printf("Error from OSSemPend Mailman");
+			return;
+		}
+		readMail();
 	}
 }
