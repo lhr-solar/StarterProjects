0ce5dfaaf1fe2838b7764560c2dd7dfeaace3daa
Madeleine Lee <madeleinercflee@gmail.com>
2022-10-02 23:51:01 +0000

    Added JSDOC @param comments, moved mutex protection so resource isn't blocked longer than necessary, changed name to item_name

--- Controls/Cafe/Customer1.c
+++ Controls/Cafe/Customer1.c
@@ -9,13 +9,17 @@
  * @brief Get the FOURTH item and cost on the menu (Cafe.h). Return the name and cost
  * of the item to the pointers passed into the function. Add the cost to the business 
  * revenue and print the current business revenue.
+ * @param item_name pointer to store what the customer buys
+ * @param cost pointer to store the cost of the purchased item
  */
-void Customer1_checkout(char** name, int* cost) {
-	*name = menu[3]; // Set value of name to fourth menu item
+void Customer1_checkout(char** item_name, int* cost) {
+	*item_name = menu[3]; // Set value of name to fourth menu item
 	*cost = costs[3]; // Set value of cost to fourth item cost
 	printf("\nCurrent business revenue: $%d\n", revenue); // Print revenue before purchase
+	OSMutexPend(&RegisterOccupied_Mutex, 0, OS_OPT_PEND_BLOCKING, &ticks, &err); // Wait for register
 	revenue += *cost; // Add value of cost to revenue
-	
+	OSMutexPost(&RegisterOccupied_Mutex, OS_OPT_POST_NONE, &err); // Release register
+	checkError(err);	
 }
 
 /**
@@ -30,11 +34,8 @@ void Task_Customer_1(void* p_arg) {
 	char* name;
 	int cost;
 	while(1){
-		OSMutexPend(&RegisterOccupied_Mutex, 0, OS_OPT_POST_NONE, &ticks, &err); // Wait for register
-		checkError(err);
 		Customer1_checkout(&name, &cost); // Pass location of name and cost to checkout
 		printf("\nCustomer 1 order: %s, $%d\n", name, cost); // Print order
-		OSMutexPost(&RegisterOccupied_Mutex, OS_OPT_POST_NONE, &err); // Release register
 		OSTimeDlyHMSM(0, 0, 4, 0, OS_OPT_TIME_HMSM_STRICT, &err); // Delay 4 sec before looping
 		checkError(err);
 	}
--- Controls/Cafe/Customer2.c
+++ Controls/Cafe/Customer2.c
@@ -9,13 +9,18 @@
  * @brief Get the FIRST item and cost on the menu (Cafe.h). Return the name and cost
  * of the item to the pointers passed into the function. Add the cost to the business 
  * revenue and print the current business revenue.
+ * @param item_name pointer to store what the customer buys
+ * @param cost pointer to store the cost of the purchased item
  */
-void Customer2_checkout(char** name, int* cost) {
-	*name = menu[0]; // Set val of name to menu[0]
-	*cost = costs[0]; // Set val of cost to costs[0]
+void Customer2_checkout(char** item_name, int* cost) {
+	*item_name = menu[0]; // Set val of name to the first menu item
+	*cost = costs[0]; // Set val of cost to the first item cost
 	printf("\nCurrent business revenue: $%d\n", revenue); // Print revenue before purchase
+	OSMutexPend(&RegisterOccupied_Mutex, 0, OS_OPT_PEND_BLOCKING, &ticks, &err); // Wait for register
+	checkError(err);
 	revenue += *cost; // Add cost of item to revenue
-	
+	OSMutexPost(&RegisterOccupied_Mutex, OS_OPT_POST_NONE, &err); // Release register
+	checkError(err);
 }
 
 /**
@@ -30,12 +35,8 @@ void Task_Customer_2(void* p_arg) {
 	char* name;
 	int cost;
 	while(1){
-		OSMutexPend(&RegisterOccupied_Mutex, 0, OS_OPT_NONE, &ticks, &err); // Wait for register
-		checkError(err);
 		Customer2_checkout(&name, &cost); // Give location of name and cost to checkout function
 		printf("\nCustomer 2 order: %s, $%d\n", name, cost); // Print order
-		OSMutexPost(&RegisterOccupied_Mutex, OS_OPT_POST_NONE, &err); // Release register
-		checkError(err);
 		OSTimeDlyHMSM(0, 0, 3, 0, OS_OPT_TIME_HMSM_STRICT, &err); // Delay 3 sec before looping
 	}
 }
