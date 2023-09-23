df3b474f57566949d9581a3a54495db0dae6877f
zoegonzalez <zgonzalez8200@gmail.com>
2023-09-23 21:08:24 +0000

    made changes to the depositletter() function

--- Controls/Mail/Mailman.c
+++ Controls/Mail/Mailman.c
@@ -13,6 +13,35 @@
  */
 void depositLetter(void) {
 	OS_ERR err; // Make sure to check for errors and print the error code if not OS_ERR_NONE
+	//goal is to print out value in mailbox address
+	
+	int i;
+	//a loop to print out the 
+	for(i = 0; i<5; i++){
+		int num = i;
+		mailbox[i] = num;
+	}
+
+	//1 sec delay
+	OSTimeDlyHMSM(0, 
+ 				  0, 
+				  1, 
+				  0,
+				  OS_OPT_TIME_HMSM_STRICT, 
+				  &err);
+
+	//to print first 5 addresses in the mailbox. or what the letter is
+	for(i = 0; i<5; i++){
+		printf(mailbox[i]);
+	}
+
+	//signal the semaphore 
+
+	
+	//finished!
+	printf("Finished!");
+
+
 }
 
 /**
@@ -22,6 +51,14 @@ void depositLetter(void) {
 void Task_Mailman(void* p_arg) {
 	OS_ERR err;	// Make sure to check for errors and print the error code if not OS_ERR_NONE
 	while(1){
+		depositLetter();
+
+		OSTimeDlyHMSM(0, 
+					  0, 
+					  3, 
+					  0,
+					  OS_OPT_TIME_HMSM_STRICT, 
+					  &err);
 
 	}
 }
