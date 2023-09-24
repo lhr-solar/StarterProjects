8e8e390d2ff87427f598951b3a2817608eaf6bfe
Aiden Madaffri <contact@aidenmadaffri.com>
2023-09-24 19:50:27 +0000

    Finish Cafe files

--- Controls/Cafe/Customer1.c
+++ Controls/Cafe/Customer1.c
@@ -1,4 +1,5 @@
 #include "Cafe.h"
+#include <stdio.h>
 
 /**
  * Customer 1 wants to buy something!
@@ -11,6 +12,14 @@
  */
 void Customer1_checkout(char** name, int* cost) {
 	OS_ERR err; // Make sure to check for errors and print the error code if not OS_ERR_NONE
+
+	// Get name and cost of the item
+	*name = menu[3];
+	// cost = costs[3];
+	*cost = costs[3];
+
+	// Add to revenue
+	revenue += *cost;
 }
 
 /**
@@ -19,11 +28,33 @@ void Customer1_checkout(char** name, int* cost) {
  */
 void Task_Customer_1(void* p_arg) {
 	OS_ERR err;	// Make sure to check for errors and print the error code if not OS_ERR_NONE
-	
+
+	// Timestamp variable for Mutex pend operations
+	CPU_TS ts;
+
 	// Use these values as parameters for Customer1_checkout
 	char* name;
 	int cost;
 	while(1){
-		
+		// Acquire Register
+		OSMutexPend(&RegisterOccupied_Mutex, 0, OS_OPT_PEND_BLOCKING, &ts, &err);
+
+		// Checkout
+		Customer1_checkout(&name, &cost);
+
+		// Print name and cost of item
+		printf("Customer 1 ordered: %s $%d\n", name, cost);
+		printf("Total revenue is now $%d\n\n", revenue);
+
+		// Release Register
+		OSMutexPost(&RegisterOccupied_Mutex, OS_OPT_POST_1, &err);
+
+		// Check for errors and print if present
+		if(err != OS_ERR_NONE) {
+			printf("Task_Customer_1 Error: %d", err);
+		}
+
+		// Delay for 4 seconds
+		OSTimeDlyHMSM(0, 0, 4, 0, OS_OPT_TIME_HMSM_STRICT, &err);
 	}
 }
--- Controls/Cafe/Customer2.c
+++ Controls/Cafe/Customer2.c
@@ -1,4 +1,5 @@
 #include "Cafe.h"
+#include <stdio.h>
 
 /**
  * Customer 2 wants to buy something!
@@ -11,6 +12,13 @@
  */
 void Customer2_checkout(char** name, int* cost) {
 	OS_ERR err; // Make sure to check for errors and print the error code if not OS_ERR_NONE
+
+	// Get name and cost of the item
+	*name = menu[0];
+	*cost = costs[0];
+
+	// Add to revenue
+	revenue += *cost;
 }
 
 /**
@@ -20,10 +28,32 @@ void Customer2_checkout(char** name, int* cost) {
 void Task_Customer_2(void* p_arg) {
 	OS_ERR err;	// Make sure to check for errors and print the error code if not OS_ERR_NONE
 
+	// Timestamp variable for Mutex pend operations
+	CPU_TS ts;
+
 	// Return the name and cost into these values from Customer2_checkout
 	char* name;
 	int cost;
 	while(1){
+		// Acquire Register
+		OSMutexPend(&RegisterOccupied_Mutex, 0, OS_OPT_PEND_BLOCKING, &ts, &err);
 
+		// Checkout
+		Customer2_checkout(&name, &cost);
+
+		// Print name and cost of item
+		printf("Customer 2 ordered: %s $%d\n", name, cost);
+		printf("Total revenue is now $%d\n\n", revenue);
+
+		// Release Register
+		OSMutexPost(&RegisterOccupied_Mutex, OS_OPT_POST_1, &err);
+
+		// Check for errors and print if present
+		if(err != OS_ERR_NONE) {
+			printf("Task_Customer_2 Error: %d", err);
+		}
+
+		// Delay for 3 seconds
+		OSTimeDlyHMSM(0, 0, 3, 0, OS_OPT_TIME_HMSM_STRICT, &err);
 	}
 }
