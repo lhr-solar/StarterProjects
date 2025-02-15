e7c450ced58b9cbf7bcb78bad185c3a130077d4b
Eliana Tejeda <you@example.com>
2025-02-15 15:39:17 -0600

    working on Mail.c

--- Controls/Mail/Mailman.c
+++ Controls/Mail/Mailman.c
@@ -8,17 +8,21 @@
 /**
  * @brief Puts a letter in the mailbox (declared in Mail.h). The letter must be unique every time
  * this function is called (suggestion: include a random number!).
- * Once the letter is deposited, signal the semaphore (effectively putting up the flag).
+ * Once the letter is deposited, signal the semapgithore (effectively putting up the flag).
  * Print "Finished!" at the end.
  */
 void depositLetter(void) {
 	OS_ERR err; // Make sure to check for errors and print the error code if not OS_ERR_NONE
+	
+	OSSemPost (&MailboxFlag_Sem4, // Posts the semaphore and points to the object being posted (icnremented)
+	OS_OPT_POST_1, // option flag that is used to wake up 1 task waiting for next availability
+	&err); // pointer to error 
 }
 
 /**
  * @brief Calls depositLetter. Delay for THREE seconds using 
  * OSTimeDlyHMSM(). Loop.
- */
+ */""
 void Task_Mailman(void* p_arg) {
 	OS_ERR err;	// Make sure to check for errors and print the error code if not OS_ERR_NONE
 	while(1){
