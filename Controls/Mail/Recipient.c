edfde4310a260f055fe3a00a203d1308095a4c34
Eliana Tejeda <you@example.com>
2025-03-28 13:39:09 -0500

    readmail written

--- Controls/Mail/Recipient.c
+++ Controls/Mail/Recipient.c
@@ -10,13 +10,15 @@
  */
 void readMail(void) {
 	OS_ERR err; // Make sure to check for errors and print the error code if not OS_ERR_NONE
+	OSTimeDly (0,0,2,0, OS_OPT_TIME_HMSM_STRICT, &err);
 	
 	if (err != OS_ERR_NONE){
-		printf ("[Mailman depositLetter()] Error Code:%d\n", err);  // if there's an error, print the error code stored in the err variable
-		return;
-	}
+			printf ("[readMail()] Error Code:%d\n", err);  // if there's an error, print the error code stored in the err variable
+			return;
+		}
+	
+	
 }
-
 /**
  * @brief Waits on the mailbox to be signaled. Once the mailbox has signaled, call readMail. Loop
  */
@@ -26,7 +28,7 @@ void Task_Recipient(void* p_arg) {
 		readMail();
 
 		if (err != OS_ERR_NONE){
-			printf("Mailman depositLetter()} Error Code:%d\n", err);
+			printf("[Mailman depositLetter()] Error Code:%d\n", err);
 		}
 		
 	}
